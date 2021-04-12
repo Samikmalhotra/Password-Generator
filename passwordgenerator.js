e1313dcd367c8b1776009bcdbc0131d13a790255
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-04-12 19:16:57 +0530

    Update passwordgenerator.js

--- passwordgenerator.js
+++ passwordgenerator.js
@@ -14,6 +14,17 @@ const randomFunc = {
     symbol: getRandomSymbol
 };
 
+generate.addEventListener('click', () => {
+	const length = +lengthEl.value;
+	const hasLower = lowercaseEl.checked;
+	const hasUpper = uppercaseEl.checked;
+	const hasNumber = numbersEl.checked;
+	const hasSymbol = symbolsEl.checked;
+	
+	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+
 const getRandomLower = () => {
     return String.fromCharCode(Math.floor(Math.random()*26) + 97);
 }
